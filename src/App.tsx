8a4f29c0aa6fcaa292d3f7c1ba498b2e410a4d30
Nate <nate6pratt@gmail.com>
2022-11-17 13:46:49 -0500

    add screen size tracking to appjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import React, { Component } from 'react'
 import PageLayout from './pages/PageLayout';
 
-function App() {
-    return <PageLayout />;
-}
+export default class App extends Component {
 
-export default App;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            windowWidth: 0,
+            windowHeight: 0
+        };
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
+    updateDimensions() {
+        const windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+        const windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+
+        console.log(`width: ${windowWidth}`);
+        console.log(`height: ${windowHeight}`);
+
+        this.setState({ windowWidth, windowHeight });
+    }
+
+    render() {
+        return (
+            <PageLayout />
+        )
+    }
+}
